90a83910608067cd8aaa4902f17b470f102ec253
Jonathan Michel <vitalzero@gmail.com>
2020-05-21 01:37:11 -0500

    Pruebas

--- MFCDialogTest/Resource.h
+++ MFCDialogTest/Resource.h
@@ -1,18 +1,18 @@
 //{{NO_DEPENDENCIES}}
-// archivo de inclusiÃ³n generado de Microsoft Visual C++.
-// Utilizado por MFCDialogTest.rc
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por MFCDialogTest.rc
 //
-#define IDR_MAINFRAME					128
-#define IDD_MFCDIALOGTEST_DIALOG				102
+#define IDD_MFCDIALOGTEST_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1001
 
-// Valores predeterminados siguientes para nuevos objetos
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
